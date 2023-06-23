5c774250dccf8a193d793510891cd4768a39bfc4
0xV4L3NT1N3 <nicholaschn@protonmail.com>
2023-06-23 16:17:32 +0800

    Hello World is back muahahahaha

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,22 +1,7 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.17;
 
-contract Counter {
-    uint public count;
-
-    // Function to get the current count
-    function get() public view returns (uint) {
-        return count;
-    }
-
-    // Function to increment count by 1
-    function inc() public {
-        count += 1;
-    }
-
-    // Function to decrement count by 1
-    function dec() public {
-        // This function will fail if count = 0
-        count -= 1;
-    }
+contract HelloWorld {
+    string public greet = "Not Hello World all over again!";
 }
