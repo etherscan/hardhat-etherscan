5b5672503664958dbdaf566d72fa1d1bc4c09631
0xV4L3NT1N3 <nicholaschn@protonmail.com>
2023-06-23 15:50:37 +0800

    Rewrite and add Etherscan verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,228 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
+require("@nomiclabs/hardhat-ethers")
+require("@nomicfoundation/hardhat-verify");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+
+  // solidity compiler settings, see more at https://docs.soliditylang.org/en/v0.8.19/using-the-compiler.html
+
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      metadata: {
+        bytecodeHash: "none",
+      },
+      optimizer: {
+        enabled: true,
+        runs: 420,
+      },
+    },
+  },
+
+  // chains, add more from this list https://chainlist.org/
+
+  networks: {
+
+    mainnet: {
+      url: process.env.MAINNET_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    kovan: {
+      url: process.env.KOVAN_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    goerli: {
+      url: process.env.GOERLI_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    rinkeby: {
+      url: process.env.RINKEBY_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    sepolia: {
+      url: process.env.SEPOLIA_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    avax: {
+      url: process.env.AVAX_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    avaxFuji: {
+      url: process.env.FUJI_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    polygon: {
+      url: process.env.POLYGON_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    polygonMumbai: {
+      url: process.env.MUMBAI_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    arbitrum: {
+      url: process.env.ARBITRUM_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    arbGoerli: {
+      url: process.env.ARBITRUM_GOERLI_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    arbNova: {
+      url: process.env.ARBITRUM_NOVA_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    goptimism: {
+      url: process.env.OPTIMISM_GOERLI_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    fantom: {
+      url: process.env.FANTOM_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    ftmTestnet: {
+      url: process.env.FANTOM_TESTNET_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    bsc: {
+      url: process.env.BSC_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    cronos: {
+      url: process.env.CRONOS_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    bttc: {
+      url: process.env.BTTC_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    gnosis: {
+      url: process.env.GNOSIS_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    baseGoerli: {
+      url: process.env.BASE_GOERLI_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    goerliPolygonzkEVM: {
+      url: process.env.POLYGONZK_GOERLI_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    polygonzkEVM: {
+      url: process.env.POLYGONZK_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    bttcDonau: {
+      url: process.env.BTTC_DONAU_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+    bobaTestnet: {
+      url: process.env.BOBA_TESTNET_PROVIDER,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+
+  // etherscan-like explorers for contract verification
+
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_API_KEY,
+      sepolia: process.env.ETHERSCAN_API_KEY,
+      bscTestnet: process.env.BSCSCAN_API_KEY,
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY,
+      ftmTestnet: process.env.FTMSCAN_API_KEY,
+      avalancheFujiTestnet: process.env.SNOWTRACE_API_KEY,
+      optimisticGoerli: process.env.OPTIMISM_API_KEY,
+      arbGoerli: process.env.ARBISCAN_API_KEY,
+      arbNova: process.env.ARBISCAN_NOVA_API_KEY,
+      gnosis: process.env.GNOSISSCAN_API_KEY,
+      baseGoerli: process.env.BASESCAN_API_KEY,
+      goerliPolygonzkEVM: process.env.POLYGONZK_API_KEY,
+      polygonzkEVM: process.env.POLYGONZK_API_KEY,
+      bttcDonau: process.env.BTTCSCAN_API_KEY,
+      bobaTestnet: process.env.BOBASCAN_API_KEY
+    },
+
+    customChains: [
+      {
+        network: "arbGoerli",
+        chainId: 421613,
+        urls: {
+          apiURL: "https://api-goerli.arbiscan.io/api",
+          browserURL: "https://goerli.arbiscan.io"
+        }
+      },
+      {
+        network: "arbNova",
+        chainId: 42170,
+        urls: {
+          apiURL: "https://api-nova.arbiscan.io/api",
+          browserURL: "https://nova.arbiscan.io"
+        }
+      },
+      {
+        network: "baseGoerli",
+        chainId: 84531,
+        urls: {
+          apiURL: "https://api-goerli.basescan.org/api",
+          browserURL: "https://goerli.basescan.org"
+        }
+      },
+      {
+        network: "goerliPolygonzkEVM",
+        chainId: 1442,
+        urls: {
+          apiURL: "https://api-testnet-zkevm.polygonscan.com",
+          browserURL: "https://testnet-zkevm.polygonscan.com"
+        }
+      },
+      {
+        network: "polygonzkEVM",
+        chainId: 1101,
+        urls: {
+          apiURL: "https://api-zkevm.polygonscan.com",
+          browserURL: "https://zkevm.polygonscan.com"
+        }
+      },
+      {
+        network: "bttcDonau",
+        chainId: 1029,
+        urls: {
+          apiURL: "https://api-testnet.bttcscan.com",
+          browserURL: "https://testnet.bttcscan.com"
+        }
+      },
+      {
+        network: "bobaTestnet",
+        chainId: 2888,
+        urls: {
+          apiURL: "https://api-testnet.bobascan.com",
+          browserURL: "https://testnet.bobascan.com"
+        }
+      }
+    ]
+  }
+
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat")
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+ 
+   // use network from hardhat command line, such as `npx hardhat run deploy.js --network goerli`
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+   const provider = new ethers.providers.Web3Provider(network.provider)
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+   // create a wallet using private key, and connect it to a node above
 
-  await lock.waitForDeployment();
+   const deployer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+   
+   console.log(`Using wallet address: ` + deployer.address)
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+   // initialise contract to be deployed
+
+   const contract = await ethers.getContractFactory("HelloWorld", [deployer])
+
+   // deploy the contract 
+
+   const deploy = await contract.deploy()
+   const receipt = await deploy.deployTransaction.wait()
+
+   console.log(`Contract successfully deployed to: ` + receipt.contractAddress)
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
